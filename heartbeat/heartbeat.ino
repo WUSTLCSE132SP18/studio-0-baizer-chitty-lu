1c2d7ca3b7821f9af3731a248052350ad1000872
sarahchitty <sarahchitty@sarahs-mbp-3.dhcp.wustl.edu>
2018-01-17 15:49:00 -0600

    studio 0 yeah!

--- heartbeat/heartbeat.ino
+++ heartbeat/heartbeat.ino
@@ -1,11 +1,17 @@
 
+const int doPin = 13;
 void setup() {
   Serial.begin(9600);
+  pinMode(doPin, OUTPUT);
 }
 
 void loop() {
-  delay(1000);
-  int time = millis()/1000 + 1;
+  int time = millis()/1000+1;
   Serial.print(time);
   Serial.println(" sec have elapsed");
+
+  digitalWrite(doPin, HIGH);
+  delay(500);
+  digitalWrite(doPin, LOW);
+  delay(500);
 }
